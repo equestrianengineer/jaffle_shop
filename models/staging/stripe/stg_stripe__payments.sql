30de8a337e1d3d3a942a7d95352af9e3fe4ca533
JAMI JOHNSON <equestrianengineer@gmail.com>
2026-02-23 12:59:43 +0000

    refactor-models-exemplar

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount / 100 as amount
+    amount / 100 as amount,
+    created as created_at
 
 from DBT_PROJECT.RAW.payments
